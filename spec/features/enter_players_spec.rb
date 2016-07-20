9f2781475fabf486f2ad496d4561a409e1acef98
6eff <6eff@mail.ru>
2016-07-20 14:42:49 +0300

    rspec for hitpoints

--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -8,4 +8,12 @@ feature "entering players form" do
     click_button ('enter')
     expect(page).to have_text('player1 vs player2')
   end
+
+  scenario "displaying player hit points" do
+    visit '/'
+    fill_in("player1", with: "player1")
+    fill_in("player2", with: "player2")
+    click_button("enter")
+    expect(page).to have_content('player1 100 vs player2 100')
+  end
 end
