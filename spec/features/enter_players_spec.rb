cc39ff339e440382a4575895cfeabbd9aacd572d
Arunas Skirius <arukomp@gmail.com>
2016-07-20 12:55:38 +0100

    Using helpers

--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -2,18 +2,12 @@ require 'spec_helper'
 
 feature "entering players form" do
   scenario "user enters his name" do
-    visit '/'
-    fill_in("player1", with: "player1")
-    fill_in('player2', with: 'player2')
-    click_button ('enter')
+    sign_in_and_play
     expect(page).to have_text('player1 vs player2')
   end
 
   scenario "displaying player hit points" do
-    visit '/'
-    fill_in("player1", with: "player1")
-    fill_in("player2", with: "player2")
-    click_button("enter")
+    sign_in_and_play
     expect(page).to have_content('player1 100 vs player2 100')
   end
 end
