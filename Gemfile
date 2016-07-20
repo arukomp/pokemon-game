34d5c80e769d41cb02fa6984fb0c23d1b7ac444e
Arunas Skirius <arukomp@gmail.com>
2016-07-20 12:19:20 +0100

    Post redirect Get pattern

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ ruby '2.3.1'
 
 gem 'capybara'
 gem 'sinatra'
+gem 'rspec'
 gem 'rspec-sinatra'
 gem 'chromedriver-helper'
--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 # require 'spec_helper'
 
 class Batle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :form
   end
 
-  post '/form' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+  post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb :play
   end
 
