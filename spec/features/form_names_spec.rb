024f79275e3b153de60738f8c3ea4bc4145b88a8
Nomi Vos <nvoswork@gmail.com>
2016-07-21 04:59:23 -0500

    21_test_helpers.md

--- spec/features/form_names_spec.rb
+++ spec/features/form_names_spec.rb
@@ -1,10 +1,6 @@
 RSpec.feature "Name Input Form", :type => :feature do
   scenario "Users enter their names" do
-    visit "/"
-
-    fill_in :player_1_name, with: "Nomi"
-    fill_in :player_2_name, with: "Julie"
-    click_button "Submit"
+    sign_in_and_play
 
     expect(page).to have_content "Nomi vs. Julie"
   end
--- spec/features/hp_display_spec.rb
+++ spec/features/hp_display_spec.rb
@@ -1,10 +1,6 @@
 RSpec.feature "Hit Points Display", :type => :feature do
   scenario "Users HPs are displayed" do
-    visit "/"
-
-    fill_in :player_1_name, with: "Nomi"
-    fill_in :player_2_name, with: "Julie"
-    click_button "Submit"
+    sign_in_and_play
 
     expect(page).to have_content "Nomi Hit Points: 60"
     expect(page).to have_content "Julie Hit Points: 60"
